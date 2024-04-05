354001e591717391697185d714928624296a2224
Andrés Choque Lopez <andreschoquelopez@gmail.com>
2024-04-05 19:25:21 -0300

    TP2.ejercicio1: sacar el assertEquals del fake object.

--- src/test/java/oo2/practico1/ejercicio1/PersistenciaTest.java
+++ src/test/java/oo2/practico1/ejercicio1/PersistenciaTest.java
@@ -11,18 +11,25 @@ class PersistenciaTest {
 
 	@Test
 	void lineaDeSalida() throws FueraDeTerminoException {
-		Persistencia probarPersistencia = new Persistencia() {
-			final String esperado = "12/12/2012 12:12:56, ParticipanteDePrueba, TestLineaDeSalida";
+		var probarPersistencia = new Persistencia() {
+
+			private String ultimaLinea;
 
 			@Override
 			public void registrarInscripcion(String lineaDeRegistro) {
-				assertEquals(esperado, lineaDeRegistro);
+				this.ultimaLinea = lineaDeRegistro;
+			}
+
+			public String getLinea() {
+				return this.ultimaLinea;
 			}
 		};
-
+		
+		final String esperado = "12/12/2012 12:12:56, ParticipanteDePrueba, TestLineaDeSalida";
 		Concurso concurso = new Concurso("TestLineaDeSalida", probarPersistencia,
 				LocalDateTime.of(2012, 12, 03, 0, 0, 0), LocalDateTime.of(2012, 12, 24, 1, 0, 0), new FechaTest(LocalDateTime.of(2012, 12, 12, 12, 12, 56)));
-
 		concurso.inscribirParticipante(new Participante("ParticipanteDePrueba"));
+
+		assertEquals(esperado, probarPersistencia.getLinea().trim());
 	}
 }
